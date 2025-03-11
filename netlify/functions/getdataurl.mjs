185eb71b4d6cf31645f589dda5e20d1239fd377f
Frida0612 <109202769+Frida0612@users.noreply.github.com>
2025-03-11 19:26:31 +0800

    Update getdataurl.mjs

--- netlify/functions/getdataurl.mjs
+++ netlify/functions/getdataurl.mjs
@@ -73,8 +73,10 @@ async function postRequest(url, body, headers = {}) {
 }
 
 // 使用示例（发送 JSON 数据）
-async function run() {
+async function run(req, context) {
   try {
+    // const fullUrl = await getRedirectedUrlWithTimeout();
+    console.log(111, req, context);
     const response = await postRequest('', querystring.stringify({
       shareParam: querystring.unescape('cZpxtFI1qCJFgY9zHyftXzCTC2i%2BunvCeRRC1nTzFRnz7vPYUiJPuIlbmYGJJUThGnhI5oO4iNsARlosVYp6NwmTRp5lpIWYY8y%2Bvs6UA7PPEH%2B8FV6q9Wx1kDZ83W9KtkptNzn8H20BxlEV%2B%2FE68b9lQew1Rp3yKrIJVE4dleE%3D')
     }));
@@ -88,6 +90,6 @@ async function run() {
 
 
 export default async (req, context) => {
-  const rst = await run();
+  const rst = await run(req, context);
   return rst;
 };
